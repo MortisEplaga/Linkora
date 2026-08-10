7d48bc0c68d0011df56dfbf16186edc35d055fbb
MortisEplaga <duleba2018@gmail.com>
2026-08-10 15:47:13 +0300

    ценники

---
+++ Linkora/Models/ProductCardViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Linkora.Models
+{
+    public class ProductCardViewModel
+    {
+        public Product Product { get; set; } = null!;
+        public bool ShowFavActions { get; set; } = true;
+        public bool ShowSeller { get; set; } = true;
+        public bool IsFav { get; set; }
+        public bool IsInCart { get; set; }
+    }
+}
