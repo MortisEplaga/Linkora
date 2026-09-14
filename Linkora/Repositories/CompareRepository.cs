9a04f8a2534582ea6fd9b58402888e43ed18752c
MortisEplaga <duleba2018@gmail.com>
2026-09-14 11:42:47 +0300

    Убрал лишний AND

--- Linkora/Repositories/CompareRepository.cs
+++ Linkora/Repositories/CompareRepository.cs
@@ -86,9 +86,8 @@ namespace Linkora.Repositories
 
                 if (row.ParamType == 4)
                 {
-                    var ids = rawValue.Split(',', StringSplitOptions.RemoveEmptyEntries);
                     var texts = new List<string>();
-                    foreach (var idStr in ids)
+                    foreach (var idStr in rawValue.Split(',', StringSplitOptions.RemoveEmptyEntries))
                         if (int.TryParse(idStr.Trim(), out int optId) && selectOptionsDict.TryGetValue(optId, out var textsTuple))
                             texts.Add(Resolve(lang, textsTuple.Value, textsTuple.ValueLV, textsTuple.ValueRU));
                         else texts.Add(idStr);
@@ -121,11 +120,10 @@ namespace Linkora.Repositories
         {
             var (inClause, parameters) = BuildInClause(paramIds, "@pid");
             var data = await QueryAsync(
-                $"SELECT Id,Value,ValueLV,ValueRU FROM SelectOptions WHERE AND ParamId IN ({inClause})",
+                $"SELECT Id,Value,ValueLV,ValueRU FROM SelectOptions WHERE ParamId IN ({inClause})",
                 r => (Id: r.GetInt32(0), Value: r.GetString(1), ValueLV: r.GetStringOrDefault(2, r.GetString(1)), ValueRU: r.GetStringOrDefault(3, r.GetString(1))),
                 p => { foreach (var prm in parameters) p.Add(prm); });
             return data.ToDictionary(x => x.Id, x => (x.Value, x.ValueLV, x.ValueRU));
         }
-
     }
 }
